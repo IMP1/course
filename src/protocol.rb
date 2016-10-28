fbf4753e7dc033f7eec6a0538466519bc0009871
IMP1 <IMP1@mail.com>
2016-10-28 09:54:51 +0100

    more/better statuses

--- src/protocol.rb
+++ src/protocol.rb
@@ -1,8 +1,19 @@
 module Protocol
     STATUS_DIGIT_LENGTH = 2
 
-    CONNECTION = 100
-    SUCCESS = 200
+    # States Categories
+    INFORMATIONAL = 100
+        CONNECTION_ESTABLISHED = 100
+    SUCCESS       = 200
+        COMMAND_EXECUTED       = 200
+    USER_ERROR    = 300
+    COURSE_ERROR  = 400
+
+    # Major States
+
+
+    # Minor States
+
 
     # TODO: work out what statuses there will be, and categorise them.
 
@@ -19,11 +30,13 @@ module Protocol
     end
 
     def self.success?(message)
-        return get_status(message)
+        status = get_status(message)
+        puts message
+        return status >= SUCCESS && status < SUCCESS + 100
     end
 
     def self.connection?(message)
-       return get_status(message) == CONNECTION
+       return get_status(message) == CONNECTION_ESTABLISHED
     end 
 
 end

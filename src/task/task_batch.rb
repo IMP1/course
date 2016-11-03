dc054fb21d98ceaaf3bbe7033a4c7b81e35ff4f3
IMP1 <IMP1@mail.com>
2016-11-03 15:06:40 +0000

    no longer calls super

--- src/task/task_batch.rb
+++ src/task/task_batch.rb
@@ -2,13 +2,11 @@ require_relative 'task'
 
 class BatchTask < Task
 
-    def initialize(batch_filename, *args)
-        super(*args)
+    def setup_task(batch_filename)
         @batch_filename = batch_filename
     end
 
     def run_task
-        super
         @result = system(@batch_filename)
     end
 

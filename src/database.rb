d6bd337ea30306ec3eeabe381848768cc5b4edbc
IMP1 <IMP1@mail.com>
2016-11-03 13:58:02 +0000

    added depenedencie and workflow tables

--- src/database.rb
+++ src/database.rb
@@ -12,22 +12,44 @@ module CourseDatabase
     end
 
     def self.setup_tables()
+        # USERS
         DB.create_table :users do
             primary_key :user_id
             String      :user_name
             String      :user_password # Plaintext? Really? Ugh.
         end
+        # ROLES
         DB.create_table :roles do
             primary_key :role_id
             String      :role_name
         end
-        DB.create_table :user_roles do
-            foreign_key :user_id, :users
-            foreign_key :role_id, :roles
-        end
         roles = DB[:roles]
         User::ROLES.each do |key, value|
             roles.insert(:role_name => value)
+        end
+        # USER ROLES
+        DB.create_table :user_roles do
+            foreign_key :user, :users
+            foreign_key :role, :roles
+        end
+        # JOBS
+        DB.create_table :jobs do
+            primary_key :job_id
+            String      :job_name
+        end
+        # WORKFLOWS
+        DB.create_table :workflows do
+            primary_key :workflow_id
+            String      :workflow_name
+            String      :workflow_description
+        end
+        # DEPENDENCIES
+        DB.create_table :dependencies do
+            foreign_key :job_id, :jobs
+            TrueClass   :dependency_is_job
+            foreign_key :dependency_job_id, :jobs
+            TrueClass   :dependency_is_workflow
+            foreign_key :dependency_workflow_id, :workflows
         end
     end
 
